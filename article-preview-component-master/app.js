56b502b4e528d11dec3e4a3c2d6e6132225f8690
Allmerr <90260916+Allmerr@users.noreply.github.com>
2022-04-02 10:45:13 +0700

    Add files via upload

---
+++ article-preview-component-master/app.js
@@ -0,0 +1,13 @@
+const sosialButton = document.querySelector('.container .profile .image-outer');
+sosialButton.addEventListener('click', ()=>{
+    const sosialEvent = document.querySelector('.container .sosialEvent');
+    sosialEvent.classList.toggle('disNone')
+});
+
+
+
+const sosialButton700 = document.querySelector('.container .image-dua')
+sosialButton700.addEventListener('click', ()=>{
+    const sosialEvent = document.querySelector('.container .sosialEvent');
+    sosialEvent.classList.toggle('disNone');
+})
